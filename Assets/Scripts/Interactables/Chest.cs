812b4dfcf7640fc1a4a4dd7b09ca059f1689aa77
Danklif <josemanrenteria@gmail.com>
2025-06-01 17:52:07 -0500

    010625-6 Mejora de los cofres

--- Assets/Scripts/Interactables/Chest.cs
+++ Assets/Scripts/Interactables/Chest.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class Chest : MonoBehaviour, IInteractable
 {
+    [SerializeField] bool unlockDash;
+    [SerializeField] bool unlockAltAttack;
+    [SerializeField] bool unlockSpecialAttack;
+
     private bool abierto = false;
     private Animator anim;
     void Start()
@@ -19,6 +23,10 @@ public class Chest : MonoBehaviour, IInteractable
             Debug.Log("¡Cofre abierto!");
             abierto = true;
             Destroy(gameObject.GetComponent<ObjectInfo>().text);
+
+            GameObject.FindGameObjectWithTag("PlayerHitbox").GetComponent<Player>().fUnlockAltAttack = unlockAltAttack;
+            GameObject.FindGameObjectWithTag("PlayerHitbox").GetComponent<Player>().fUnlockSpecialAttack = unlockSpecialAttack;
+            GameObject.FindGameObjectWithTag("PlayerHitbox").GetComponent<Player>().fUnlockDash = unlockDash;
         }
     }
 }
